de4459b0a2c71da771b560bda253cf38b99c34f1
Rishi Vishwakarma <rishivishwakarma24x7@gmail.com>
2017-04-04 12:30:08 +0530

    code updated with form action

--- index.php
+++ index.php
@@ -4,19 +4,20 @@
   <title>Multiple File Upload with PHP</title>
 </head>
 <body>
- 
+
   <h2>Upload Files:</h2>
   <!-- IMPORTANT:  FORM's enctype must be "multipart/form-data" -->
   <form method="post" action="upload.php" enctype="multipart/form-data">
-    <input type="file" name="files" id="files" multiple="" onChange="makeFileList();" accept="image/*" />
-  </form>	
+    <input type="file" name="fileToUpload" id="files" multiple="" onChange="makeFileList();" accept="image/*" />
+    <input type="submit" value="upload" name="upload">
+  </form>
 
   <p>
     <strong>Files You Selected:</strong>
   </p>
 
   <ul id="fileList"><li>No Files Selected</li></ul>
-  
+
   <script>
     function makeFileList() {
       var input = document.getElementById("files");
@@ -26,7 +27,8 @@
       }
       for (var i = 0; i < input.files.length; i++) {
         var li = document.createElement("li");
-        li.innerHTML = input.files[i].name;
+        console.log(input.files);
+        li.innerHTML = '<img src="'+input.files[i].name+'" alt="'+input.files[i].name+'">';
         ul.appendChild(li);
       }
       if(!ul.hasChildNodes()) {
@@ -38,25 +40,3 @@
   </script>
 </body>
 </html>
-<?php
-
-session_start();
-
-if (isset($_POST["submit"])){
-	if(count($_FILES['file']['name'])){     
-    //This code uploads the files into 'uploads' directory inside the root directory
-    $target="uploads/";               
-    $count=0;
-
-    foreach ($_FILES['file']['name'] as $filename){
-        $temp=$target;
-        $tmp=$_FILES['file']['tmp_name'][$count];
-        $count=$count + 1;
-        $temp=$temp.basename($filename);
-        move_uploaded_file($tmp,$temp);
-        $temp='';
-        $tmp='';
-    }//foreach
-	}//countif
-}//submit
-?>
