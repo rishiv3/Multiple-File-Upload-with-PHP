5f68d05115e5d83ab222d0271501b5fe4a68e59b
Rishi Vishwakarma <rishivishwakarma24x7@gmail.com>
2017-04-10 11:50:24 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -36,6 +36,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
+    // uploading file to the desired directory
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
